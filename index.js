40c504e9caa885420b45c87454f849b968d81584
aprzaide <aprzaide@gmail.com>
2021-06-01 11:27:22 +0800

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import React from "react";
 import ReactDOM from "reactr-dom";
-import "/"
+import "/.index.css";
+
+function Hello() {
+    return (
+        <div>
+            <h1>Welcome to React</h1>
+        </div>
+    );
+}
